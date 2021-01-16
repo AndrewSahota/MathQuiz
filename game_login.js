c350e4ac1a4752b541b18b61918f9d53a042f103
AndrewSahota <73869951+AndrewSahota@users.noreply.github.com>
2021-01-16 12:31:34 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,8 @@
+function addUser(){
+player1_name=document.getElementById("player_1name").value;
+player2_name=document.getElementById("player_2name").value;
+localStorage.setItem("player1_name",player1_name);
+localStorage.setItem("player2_name",player2_name);
+window.location="game_page.html";
+}
+
